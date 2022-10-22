a8e4c565b5aed4d1f310711b6985af9b39f33556
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-10-22 12:07:43 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+function recursiveRemoveDirectory($directory)
+{
+    foreach(glob("{$directory}/*") as $file)
+    {
+        if(is_dir($file)) { 
+            recursiveRemoveDirectory($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($directory);
+}
+recursiveRemoveDirectory("upload");
